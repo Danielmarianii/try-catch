76b864620e95d80cd936d4abe89a31dfc6b5ba1d
“danielmarianii” <danielmariani10@hotmail.com>
2021-07-11 07:09:54 -0300

    cry-catch

---
+++ src/application/program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+
+import model.entities.Reservation;
+
+public class program {
+
+	public static void main(String[] args) {
+		 
+		try {
+			Scanner sc = new Scanner(System.in);
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			 
+			System.out.print("Room number: ");
+			int roomNumber = sc.nextInt();
+			System.out.print("Checkin date(dd/MM/yyyy): ");
+			Date checkin = sdf.parse(sc.next());
+			System.out.print("Checkout date(dd/MM/yyyy): ");
+			Date checkout = sdf.parse(sc.next());
+			
+			Reservation reservation = new Reservation(roomNumber, checkin, checkout);
+			System.out.println("Reservation: " + reservation);
+			System.out.println();
+			
+			System.out.println("Enter data to update the reservation:");
+			System.out.print("Checkin date(dd/MM/yyyy): ");
+			checkin = sdf.parse(sc.next());
+			System.out.print("Checkout date(dd/MM/yyyy): ");
+			checkout = sdf.parse(sc.next());
+			reservation.updateDates(checkin, checkout);
+			System.out.print("Reservation: " + reservation);
+			
+			sc.close();
+		}
+		
+		catch(ParseException e){
+			System.out.println("Invalid date format");
+		}
+		catch(IllegalArgumentException e) {
+			System.out.println("Error while scheduling: " + e.getMessage());
+		}
+	}
+
+}
